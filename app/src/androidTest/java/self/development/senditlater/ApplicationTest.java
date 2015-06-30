7d0db7c452a493af3a7f9dabf3523bd708c46c71
antoniomaia <antoniomaia@A.local>
2015-07-01 01:13:24 +0200

    Initial commit

---
+++ app/src/androidTest/java/self/development/senditlater/ApplicationTest.java
@@ -0,0 +1,13 @@
+package self.development.senditlater;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
